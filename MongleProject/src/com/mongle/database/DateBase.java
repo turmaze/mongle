efefe3fb48ded6e8b72c2b27691efad5e443db18
turmaze <150693016+turmaze@users.noreply.github.com>
2024-01-26 12:35:14 +0900

    default
    asdada

---
+++ MongleProject/src/com/mongle/database/DateBase.java
@@ -0,0 +1,39 @@
+package com.mongle.database;
+
+import java.util.ArrayList;
+
+public class DateBase {
+	
+	private static void dataSave() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private static void dataLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private static void validTest(String object) {
+		//유효성 검사
+		/*	
+		 	이름	(name)		- Private String
+		 	은행 (bank) 		- Private String
+		 	계좌번호 (acc) 	- Private String
+		 	전화번호 (tel) 	- Private String
+		 	생년월일 (birth) - Private String
+		 	주소 (address) 	- Private String
+		 	아이디 (id) 		- Private String
+		 	비밀번호 (pw) 	- Private String
+		 */
+	
+	}
+
+	private static void get(ArrayList userData) {
+		//호출문
+		
+	}
+	
+}
---
+++ MongleProject/src/com/mongle/main/Main.java
@@ -0,0 +1,16 @@
+package com.mongle.main;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}//main
+}
