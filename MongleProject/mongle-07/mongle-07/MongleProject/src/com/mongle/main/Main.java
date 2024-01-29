aa62c57521d6bf416e433ee10e91cbf0ff2a4ee6
Kim0f85 <155717265+Kim0f85@users.noreply.github.com>
2024-01-29 16:12:00 +0900

    update0129
    update0129
    -ACC check update

---
+++ MongleProject/mongle-07/mongle-07/MongleProject/src/com/mongle/main/Main.java
@@ -0,0 +1,19 @@
+package com.mongle.main;
+
+import com.mongle.yourapp.SignUp;
+
+public class Main {
+	public static void main(String[] args) {
+		SignUp.signUp();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}//main
+}
---
+++ MongleProject/mongle-07/mongle-07/MongleProject/src/com/mongle/test/SignUpTest.java
@@ -0,0 +1,15 @@
+package com.mongle.test;
+
+import java.util.Scanner;
+
+import com.mongle.view.MongleVisual;
+
+public class SignUpTest {
+	public static void main(String[] args) {
+		//Scanner scan = new Scanner(System.in);
+		
+		//System.out.print("test input: ");
+		MongleVisual.menuHeader("회원가입");
+		
+	}
+}
