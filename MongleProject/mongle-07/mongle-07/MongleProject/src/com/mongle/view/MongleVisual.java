c2538598c64aa74df75203873d736411cece59b6
Kim0f85 <155717265+Kim0f85@users.noreply.github.com>
2024-01-29 16:34:12 +0900

    update 0129
    유효성 검사 추가 
    - ACC 유효성 검사 완료 
    - 은행명 설정중 
    - 이름 (완료)
    - 생일(완료)

---
+++ MongleProject/mongle-07/mongle-07/MongleProject/src/com/mongle/view/MongleVisual.java
@@ -0,0 +1,49 @@
+package com.mongle.view;
+
+public class MongleVisual {
+
+	private static void menuList(String menu, int menuType) {
+		// menuType 0 -> 가운데 + 세로 한줄
+		// menuType 1 -> 가운데 + 세로 두줄
+		// menuType 2 -> 위에 + 세로 한줄
+		
+		// menu -> 변수명 ex) 투자관리, 대출관리
+		
+		
+		
+	}
+
+	public static void menuHeader(String titleName) {
+		// 메뉴 헤더 화면
+		System.out.printf("%22s===================================\n"," ");
+		System.out.printf("%40s\n",titleName);
+		System.out.printf("%22s===================================\n"," ");
+		
+	}
+
+	private static void menuPage() {
+		// 로그인 후 메뉴 화면
+	}
+
+	private static void findAccPage() {
+		//계정 찾기 화면
+		
+	}
+
+	private static void signUpPage() {
+		//회원가입 화면
+	}
+
+	private static void loginPage() {
+		//로그인 화면
+	}
+
+	private static void startPage() {
+		//첫 실행화면
+		
+	}
+	
+	
+	
+	
+}
