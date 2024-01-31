984fcebe7eddf10e39557c480cb820eaa657cc24
bread0808 <155510771+bread0808@users.noreply.github.com>
2024-02-01 08:30:48 +0900

    token

--- MongleProject/src/com/mongle/main/Main.java
+++ MongleProject/src/com/mongle/main/Main.java
@@ -6,15 +6,18 @@ import java.io.IOException;
 import org.json.simple.parser.ParseException;
 
 import com.mongle.database.DataBase;
+import com.mongle.service.invest.Stock;
 import com.mongle.yourapp.LogIn;
 import com.mongle.yourapp.SignUp;
 
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
-		DataBase.dataLoad();
+		//DataBase.dataLoad();
 		//LogIn.LogIn();
 		//MainMenu.MainMenu();
-		SignUp.signUp();
+		//SignUp.signUp();
+		
+		Stock.stockService();
 		
 		
 		
