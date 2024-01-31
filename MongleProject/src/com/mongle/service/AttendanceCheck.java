fd2423302305920fd7c9da170436b884b649e5b2
bread0808 <155510771+bread0808@users.noreply.github.com>
2024-02-01 08:53:17 +0900

    attend

--- MongleProject/src/com/mongle/service/AttendanceCheck.java
+++ MongleProject/src/com/mongle/service/AttendanceCheck.java
@@ -1,11 +1,35 @@
 package com.mongle.service;
 
+import java.util.Scanner;
+
 import com.mongle.view.MongleVisual;
 
 public class AttendanceCheck {
 	
 	public static void attendanceCheckService() {
-		MongleVisual.menuHeader("출석 체크");
+		Scanner scan = new Scanner(System.in);
+
+		while (true) {
+			MongleVisual.menuHeader("출석 체크");
+
+			System.out.println();
+			
+			
+			
+			System.out.println();
+
+			System.out.printf("%22s9. 홈으로", " ");
+			System.out.printf("%22s0. 이전으로", " ");
+
+			String sel = scan.nextLine();
+			if (sel.equals("1")) {
+
+			} else if (sel.equals("9")) {
+				break;
+			} else if (sel.equals("0")) {
+				break;
+			}
+		}
 	}
 	
 }
