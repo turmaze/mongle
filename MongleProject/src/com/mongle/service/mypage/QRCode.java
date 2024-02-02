c249ce2e9e0498e10b6b2966d38713d2428c9fbd
Heey2 <shannon.hykim@gmail.com>
2024-02-02 15:47:31 +0900

    수정

--- MongleProject/src/com/mongle/service/mypage/QRCode.java
+++ MongleProject/src/com/mongle/service/mypage/QRCode.java
@@ -1,10 +1,12 @@
 package com.mongle.service.mypage;
 
+import java.util.Scanner;
+
 import com.mongle.view.MongleVisual;
 
 public class QRCode {
-	public static void qr() {
-		
+	public static int qr() {
+		Scanner scan = new Scanner(System.in);
 		MongleVisual.menuHeader("결제용 QR코드");
 		try {
 			ProcessBuilder builder = new ProcessBuilder();
@@ -14,5 +16,23 @@ public class QRCode {
 			System.out.println("qr.main");
 			e.printStackTrace();
 		}
+
+		boolean loop = true;
+		while (loop) {
+
+			System.out.printf("%22s9. 홈으로\n", " ");
+			System.out.printf("%22s0. 이전으로\n", " ");
+			System.out.println();
+
+			System.out.printf("%22s선택(번호) :", " ");
+			String sel = scan.nextLine();
+
+			if (sel.equals("9")) {
+				return 9;
+			} else if (sel.equals("0")) {
+				return 0;
+			}
+		}
+		return 0;
 	}
 }
