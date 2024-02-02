62474231a4de00a05de56f9448ddf3489665ce5e
Kim0f85 <shk19990314@gmail.com>
2024-02-02 12:41:26 +0900

    update0202(12:41)
    findacc

--- MongleProject/src/com/mongle/yourapp/FindAcc.java
+++ MongleProject/src/com/mongle/yourapp/FindAcc.java
@@ -1,6 +1,8 @@
 package com.mongle.yourapp;
 
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
@@ -9,35 +11,37 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import com.mongle.database.DataBase;
 import com.mongle.resource.ResourcePath;
 import com.mongle.resource.UserData;
 import com.mongle.view.MongleVisual;
 
 public class FindAcc {
 	public static void findAcc() {
-		Scanner scan = new Scanner(System.in); 
-		
+		Scanner scan = new Scanner(System.in);
+
 		MongleVisual.pusher();
 		MongleVisual.menuHeader("아이디 / 비밀번호 찾기");
 
-		
 		System.out.printf("\n%22s1. 아이디 찾기", " ");
 		System.out.printf("\n%22s2. 비밀번호 찾기", " ");
 		System.out.printf("\n%22s0. 이전으로", " ");
-		
+
 		String choice = scan.nextLine();
-		
-		if(choice.equals("1")) {
+
+		if (choice.equals("1")) {
 			findMyId();
-		}else if (choice.equals("2")) {
+		} else if (choice.equals("2")) {
 			findMyPw();
-		}else if (choice.equals("0")) {
+		} else if (choice.equals("0")) {
 			StartPage.startPage();
 		}
 	}
+
 	private static void findMyPw() {
 		UserData user = new UserData();
 		Scanner scan = new Scanner(System.in);
+		boolean loop = true;
 		try {
 			System.out.printf("\n%22s아이디(ID) 입력: ", " ");
 			user.setId(scan.nextLine());
@@ -45,31 +49,49 @@ public class FindAcc {
 			user.setPhone(scan.nextLine());
 			System.out.printf("\n%22s이름 입력: ", " ");
 			user.setName(scan.nextLine());
+			JSONParser parser = new JSONParser();
+			ArrayList<HashMap> list = DataBase.getUser();
 			do {
-				JSONParser parser = new JSONParser();
-				JSONArray list = (JSONArray) parser.parse(new FileReader(ResourcePath.MEMBER));
 				String checkID = "";
 				String checkPhone = "";
 				String checkName = "";
-				String findPw = "";
-				for (Object obj : list) {
-					if (((JSONObject)obj).get("id").equals(user.getId()) ) {
-						findPw = (String)((JSONObject)obj).get("pw"); 
-						checkPhone = (String) ((JSONObject)obj).get("phone");
-						checkID = (String) ((JSONObject)obj).get("id");
-						checkName = (String) ((JSONObject)obj).get("name");
+				for (HashMap obj : list) {
+
+					if (obj.get("id").equals(user.getId())) {
+						checkPhone = (String) (obj).get("phone");
+						checkID = (String) (obj).get("id");
+						checkName = (String) (obj).get("name");
+
+						if (user.getName().equals(checkName) && user.getId().equals(checkID)
+								&& user.getPhone().equals(checkPhone)) {
+							System.out.printf("\n%22s변경할 비밀번호 입력: ", " ");
+							String setPW = scan.nextLine();
+							System.out.printf("\n%22s변경할 비밀번호 다시입력: ", " ");
+							String checksetPW = scan.nextLine();
+							if (setPW.equals(checksetPW)) {
+								System.out.println(obj);
+								Encrypt encrypt = new Encrypt();
+								String finPw = Encrypt.encrypt(setPW);
+								(obj).replace("salt", Encrypt.AbcSalt);
+								(obj).replace("pw", finPw);
+								System.out.println(obj);
+								System.out.printf("\n%22s비밀번호 변경완료", " ");
+								loop = false;
+								break;
+							}else {
+								System.out.printf("\n%22s다시입력하세요\n", " ");
+							}
+						}
+						break;
 					}
 				}
-				if(user.getName().equals(checkName)&&user.getId().equals(checkID)&&user.getPhone().equals(checkPhone)) {
-					System.out.printf("\n%22s비밀번호: %s", " ",findPw);
-					break;
-				}
-			}while(true);
+			} while (loop);
 		} catch (Exception e) {
 			System.out.println("FindAcc.findAcc");
 			e.printStackTrace();
 		}
 	}
+
 	private static void findMyId() {
 		UserData user = new UserData();
 		Scanner scan = new Scanner(System.in);
@@ -78,28 +100,28 @@ public class FindAcc {
 			user.setPhone(scan.nextLine());
 			System.out.printf("\n%22s이름 입력: ", " ");
 			user.setName(scan.nextLine());
+			JSONParser parser = new JSONParser();
+			JSONArray list = (JSONArray) parser.parse(new FileReader(ResourcePath.MEMBER));
 			do {
-				JSONParser parser = new JSONParser();
-				JSONArray list = (JSONArray) parser.parse(new FileReader(ResourcePath.MEMBER));
 				String checkPhone = "";
 				String checkName = "";
-				String findPw = "";
+				String findID = "";
 				for (Object obj : list) {
-					if (((JSONObject)obj).get("id").equals(user.getId()) ) {
-						findPw = (String)((JSONObject)obj).get("pw"); 
-						checkPhone = (String) ((JSONObject)obj).get("phone");
-						checkName = (String) ((JSONObject)obj).get("name");
+					if (((JSONObject) obj).get("name").equals(user.getName())) {
+						findID = (String) ((JSONObject) obj).get("id");
+						checkPhone = (String) ((JSONObject) obj).get("phone");
+						checkName = (String) ((JSONObject) obj).get("name");
 					}
 				}
-				if(user.getName().equals(checkName)&&user.getPhone().equals(checkPhone)) {
-					System.out.printf("\n%22s비밀번호: %s", " ",findPw);
+				if (user.getName().equals(checkName) && user.getPhone().equals(checkPhone)) {
+					System.out.printf("\n%22s아이디: %s", " ", findID);
 					break;
 				}
-			}while(true);
+			} while (true);
 		} catch (Exception e) {
 			System.out.println("FindAcc.findAcc");
 			e.printStackTrace();
 		}
 	}
-	
+
 }
