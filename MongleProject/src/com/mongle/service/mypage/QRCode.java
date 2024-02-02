d0a74cf68c6273b5e50c6236c88f76fda1f4d8be
Heey2 <shannon.hykim@gmail.com>
2024-02-02 15:38:24 +0900

    qr 임시

--- MongleProject/src/com/mongle/service/mypage/QRCode.java
+++ MongleProject/src/com/mongle/service/mypage/QRCode.java
@@ -1,5 +1,18 @@
 package com.mongle.service.mypage;
 
-public class QRCode {
+import com.mongle.view.MongleVisual;
 
+public class QRCode {
+	public static void qr() {
+		
+		MongleVisual.menuHeader("결제용 QR코드");
+		try {
+			ProcessBuilder builder = new ProcessBuilder();
+			builder.command("mspaint.exe", "dat\\qr.png");
+			builder.start();
+		} catch (Exception e) {
+			System.out.println("qr.main");
+			e.printStackTrace();
+		}
+	}
 }
