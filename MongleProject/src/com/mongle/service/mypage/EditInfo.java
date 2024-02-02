cae2278fa54f0c3c9003a8a94027c1d0917a0d8a
Heey2 <shannon.hykim@gmail.com>
2024-02-02 10:47:15 +0900

    editinfo 업데이트

--- MongleProject/src/com/mongle/service/mypage/EditInfo.java
+++ MongleProject/src/com/mongle/service/mypage/EditInfo.java
@@ -18,10 +18,11 @@ public class EditInfo {
 		}
 		
 		System.out.printf("%22s아이디 : %s\n", " ", userData.get("id"));
-		System.out.printf("%22s비밀번호 : %s\n", " ", userData.get("pw"));
 		System.out.printf("%22s이름 : %s\n", " ", userData.get("name"));
 		System.out.printf("%22s생년월일 : %s\n", " ", userData.get("birth"));
 		System.out.printf("%22s전화번호 : %s\n", " ", userData.get("phone"));
+		System.out.println();
+		
 		
 	}
 }
