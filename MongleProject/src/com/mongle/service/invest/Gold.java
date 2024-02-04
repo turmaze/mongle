15530e240df3afbf254e470066534a92c5cd6878
Heey2 <155238712+Heey2@users.noreply.github.com>
2024-02-04 17:41:46 +0900

    주석 처리 추가

--- MongleProject/src/com/mongle/service/invest/Gold.java
+++ MongleProject/src/com/mongle/service/invest/Gold.java
@@ -16,28 +16,58 @@ import com.mongle.resource.Investment;
 import com.mongle.service.InvestService;
 import com.mongle.view.MongleVisual;
 
+/**
+ * 금 상품 클래스
+ */
 public class Gold {
-
+	/**
+	 * 금 상품 보유 내역
+	 */
 	public static ArrayList<Investment> listGold = new ArrayList<>();
 	private static int price;
 	private static int num;
 
+	/**
+	 * 매입한 금 가격 Getter
+	 * 
+	 * @return 매입한 금 가격
+	 */
 	public double getPrice() {
 		return price;
 	}
 
+	/**
+	 * 매입한 금 가격 Setter
+	 * 
+	 * @param price 매입한 금 가격
+	 */
 	public void setPrice(int price) {
 		this.price = price;
 	}
 
+	/**
+	 * 매입한 금 수량 Getter
+	 * 
+	 * @return 매입한 금 수량
+	 */
 	public int getNum() {
 		return num;
 	}
 
+	/**
+	 * 매입한 금 수량 Setter
+	 * 
+	 * @param num 매입한 금 수량
+	 */
 	public void setNum(int num) {
 		this.num = num;
 	}
 
+	/**
+	 * 금 상품 시세 검색 및 주문
+	 * 
+	 * @return 메뉴 이동을 위한 변수
+	 */
 	public static int goldService() {
 		Scanner scan = new Scanner(System.in);
 		List<InfoProduct> table = new ArrayList<>(); // 상품 정보 리스트
@@ -45,11 +75,11 @@ public class Gold {
 		int r = -1;
 
 		while (loop) {
-			
+
 			MongleVisual.pusher();
-			
+
 			MongleVisual.menuHeader("금");
-			
+
 			table = searchGold(table); // 금상품 검색 메서드
 
 			System.out.println();
@@ -62,9 +92,9 @@ public class Gold {
 				String sel = scan.nextLine();
 				if (sel.equals("1")) {
 					Gold gold = new Gold();
-					gold.setPrice((int)(table.get(0).getRate() + table.get(0).getMaxRate()) / 2);
+					gold.setPrice((int) (table.get(0).getRate() + table.get(0).getMaxRate()) / 2);
 					System.out.println();
-					gold.setNum(orderGold(gold.price, scan));
+					gold.setNum(orderGold(gold.price));
 					loop = false;
 				} else if (sel.equals("9")) {
 					MongleVisual.menuMove("홈 화면");
@@ -77,15 +107,22 @@ public class Gold {
 				}
 			} // while
 		} // while
-		
-		//listGold.add(new Investment("금","금",price,num));
-		//Investment.list.add(listGold);
-		Investment.list.add(new Investment("금","금","금",price,num));
-		
+
+		// listGold.add(new Investment("금","금",price,num));
+		// Investment.list.add(listGold);
+		Investment.list.add(new Investment("금", "금", "금", price, num));
+
 		return 0;
 	}
 
-	public static int orderGold(int price, Scanner scan) {
+	/**
+	 * 금 상품 주문
+	 * 
+	 * @param price 매입할 금 가격
+	 * @return 매입할 금 수량
+	 */
+	public static int orderGold(int price) {
+		Scanner scan = new Scanner(System.in);
 		boolean loop = true;
 		MongleVisual.pusher();
 		System.out.printf("%22s===================================\n", " ");
@@ -99,7 +136,6 @@ public class Gold {
 
 					InvestService.transaction("금상품 구매", price, Integer.parseInt(num));
 
-					
 					return Integer.parseInt(num);
 				}
 			} catch (NumberFormatException e) {
@@ -109,6 +145,12 @@ public class Gold {
 		return 0;
 	}
 
+	/**
+	 * 금 상품 시세 검색
+	 * 
+	 * @param table 금 상품 시세 정보를 담을 리스트
+	 * @return 금 상품 시세 정보를 담은 리스트
+	 */
 	private static List<InfoProduct> searchGold(List<InfoProduct> table) {
 		String header = "+------------+---------------+----------+-----------+";
 		System.out.printf("%11s%s\n", " ", header);
@@ -134,7 +176,7 @@ public class Gold {
 				InfoProduct d = new InfoProduct("", title, period, rate, maxrate);
 				table.add(d);
 			}
-			printAsciiTable(table);
+			print(table);
 		} catch (Exception e) {
 			System.out.println("emain");
 			e.printStackTrace();
@@ -144,13 +186,23 @@ public class Gold {
 		return table;
 	}
 
-	public static void printAsciiTable(List<InfoProduct> data) { // 표에 반복해서 출력하는 메서드
+	/**
+	 * 표 형식으로 출력
+	 * @param data 출력할 리스트
+	 */
+	public static void print(List<InfoProduct> data) {
 		for (int i = 0; i < 10; i += 2) {
 			System.out.printf("%11s|%-12s|%-12s\t|%9s원|%9s원|\n", " ", data.get(i).getPeriod(), data.get(i).getTitle(),
 					data.get(i).getMaxRate(), data.get(i).getRate());
 		}
 	}
 
+	/**
+	 * XML API 분석 및 내용 추출
+	 * @param tag 필요한 내용 명칭
+	 * @param eElement 내용을 찾아낼 Element 
+	 * @return 찾아낸 정보
+	 */
 	public static String getTagValue(String tag, Element eElement) {
 		String result = "";
 		NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
