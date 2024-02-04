fe1904db613b84988528d75525ada898080cb6cd
Heailey <158002759+Heailey@users.noreply.github.com>
2024-02-04 09:26:20 +0900

    WireTransfer

---
+++ MongleProject/src/com/mongle/main/TestMain03.java
@@ -0,0 +1,34 @@
+package com.mongle.main;
+
+import com.mongle.database.DataBase;
+import com.mongle.resource.BankAccount;
+import com.mongle.resource.Investment;
+import com.mongle.resource.ResourcePath;
+import com.mongle.sign.SignUp;
+import com.mongle.yourapp.LogIn;
+
+public class TestMain03 {
+
+	public static void main(String[] args) {
+		DataBase.dataLoad();
+//		DataBase.dataLoad(ResourcePath.ADMIN);
+		
+		
+	//	SignUp.signUp();
+
+	 LogIn.logIn();
+	//	DataBase.loadPrivateUser(LogIn.primaryKey);
+//		InvestService.investMenu();
+
+		// MainMenu.MainMenu();
+
+		// Stock.stockService();
+		// AttendanceCheck.attendanceCheckService();
+	//	System.out.println(Investment.list);
+		DataBase.changeData(DataBase.getUser(), "account", BankAccount.list); // sample
+		DataBase.changeData(DataBase.getUser(), "invest", Investment.list); // sample
+		DataBase.dataSave();
+//		DataBase.dataSave(ResourcePath.ADMIN);
+	}
+
+}
