7272a95e7d251d4f39cf57e431ee991e40307e08
kmggmk <127924216+kmggmk@users.noreply.github.com>
2024-02-05 15:21:21 +0900

    수수료 계산기 구현

--- MongleProject/src/com/mongle/loan/Loan.java
+++ MongleProject/src/com/mongle/loan/Loan.java
@@ -123,7 +123,7 @@ public class Loan {
 			System.out.printf("%s\n", header);
 			System.out.printf("|      대출명    \t|       원금   \t|  이자율  |  대출기간 | 남은기간  |\n", " ");
 			System.out.printf("%s\n", header);
-			printLoanTable(loanlist); // json 에서 가져온 데이터
+			printLoanTable(loanlist); 
 			System.out.printf("%s\n", header);
 
 			System.out.printf("%22s9. 홈으로\n", " ");
@@ -140,7 +140,7 @@ public class Loan {
 
 	}
 	
-	public static void printLoanTable(ArrayList<Loan> data) { // 표에 반복해서 출력하는 메서드
+	public static void printLoanTable(ArrayList<Loan> data) { 
 		for (int i = 0; i < data.size(); i++) {
 			System.out.printf("|%-9s\t|%,12d원\t|%6.1f%% |%5d개월 |%5d개월 |\n",  data.get(i).getloanName(),
 					data.get(i).getprincipal(), data.get(i).getRate(), data.get(i).getloanPeriod(),data.get(i).getrPeriod());
@@ -155,6 +155,7 @@ public class Loan {
 		int exten = 0;
 		System.out.printf("%22s대출을 연장 하시겠습니까?(y/n)"," ");
 		sel = sc.nextLine();
+	
 		if(sel.equals("y") || sel.equals("Y")) {
 			//연장
 			System.out.printf("%22s기간을 몇 개월 연장 하시겠습니까?"," ");
@@ -165,9 +166,43 @@ public class Loan {
 			
 			
 		}else {
+			System.out.printf("%22s9. 홈으로\n", " ");
+			System.out.printf("%22s0. 이전으로\n", " ");
+			MongleVisual.choiceGuidePrint();
 			System.out.printf("%22s이전 화면으로 돌아갑니다."," ");
 			
 		}
+	
+		
+	}
+
+
+	public static void calculrator() {
+		//  중도 상환 수수료 계산기
+		String header = "+---------------+---------------+--------+---------+--------+";
+		System.out.printf("%s\n", header);
+		System.out.printf("|      대출명    \t|       원금   \t|  이자율  |  대출기간 | 남은기간  |\n", " ");
+		System.out.printf("%s\n", header);
+		printLoanTable(loanlist); 
+		System.out.printf("%s\n", header);
+		System.out.println();
+		int a = loanlist.get(0).getloanPeriod();
+		int b = loanlist.get(0).getrPeriod();
+		int c = loanlist.get(0).getprincipal();
+		
+		int result = a-b;
+		
+		if(result <= 3) {
+			System.out.printf("%22s %s을 중도 상환시 수수료 %,d원 입니다\n. "," ",loanlist.get(0).getloanName(),c/100);
+		}
+		else if(result > 3 && result <= 6) {
+			System.out.printf("%22s %s을 중도 상환시 수수료 %,d원 입니다\n. "," ",loanlist.get(0).getloanName(),(c/100)/2);
+		}
+		else {
+			System.out.printf("%22s %s을 6개월 이상 상환 하셔서 수수료가 없습니다 ! \n"," ",loanlist.get(0).getloanName());
+		}
+		
+		
 		
 		
 	}
--- MongleProject/src/com/mongle/view/LoanView.java
+++ MongleProject/src/com/mongle/view/LoanView.java
@@ -16,7 +16,7 @@ public class LoanView {
 			System.out.println();
 			System.out.printf("%22s 1.대출정보\n", " ");
 			System.out.printf("%22s 2.대출연장\n", " ");
-			System.out.printf("%22s 3.중도/전체 상환\n", " ");
+			System.out.printf("%22s 3.중도 상환 수수료 계산기\n", " ");
 			System.out.printf("%22s 0.이전으로\n", " ");
 			System.out.printf("%22s번호를 입력하세요:", " ");
 			
@@ -25,13 +25,13 @@ public class LoanView {
 			
 			if(sel.equals("1")) {
 				// 대출정보
-				Loan.checkLoan();
-				
+				Loan.checkLoan();				
 			}else if(sel.equals("2")) {
 				// 대출연장
 				Loan.extension();
 			}else if(sel.equals("3")) {
 				// 중도/전체 상환
+				Loan.calculrator();
 			}else if(sel.equals("0")) {
 				break;
 				//
