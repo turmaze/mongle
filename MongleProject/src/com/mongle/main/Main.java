dff8fe8a9b4f2c411dab6b1189afa7b4f34980a3
bread0808 <155510771+bread0808@users.noreply.github.com>
2024-02-05 09:59:50 +0900

    메인클래스 수정

--- MongleProject/src/com/mongle/main/Main.java
+++ MongleProject/src/com/mongle/main/Main.java
@@ -11,29 +11,19 @@ import com.mongle.resource.Investment;
 import com.mongle.resource.ResourcePath;
 import com.mongle.sign.SignUp;
 import com.mongle.yourapp.LogIn;
+import com.mongle.yourapp.StartPage;
 
 public class Main {
-	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+	
+	public static void main(String[] args) {
+		programRun();
+	}
+
+	private static void programRun() {
 		DataBase.dataLoad();
-//		DataBase.dataLoad(ResourcePath.ADMIN);
-		
-		
-		SignUp.signUp();
-
-		// LogIn.logIn();
-		DataBase.loadPrivateUser(LogIn.primaryKey);
-//		InvestService.investMenu();
-
-		// MainMenu.MainMenu();
-
-		// Stock.stockService();
-		// AttendanceCheck.attendanceCheckService();
-		System.out.println(Investment.list);
-		DataBase.changeData(DataBase.getUser(), "계좌", BankAccount.list); // sample
-		DataBase.changeData(DataBase.getUser(), "투자", Investment.list); // sample
+		StartPage.startPage();
 		DataBase.dataSave();
-//		DataBase.dataSave(ResourcePath.ADMIN);
-		
-
-	}// main
+	}
+	
 }
+
