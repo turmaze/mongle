b8a2147b1141bbd3b14888d1cfeffd7613afb384
Heey2 <155238712+Heey2@users.noreply.github.com>
2024-02-05 12:14:19 +0900

    주석 처리 추가

--- MongleProject/src/com/mongle/service/CustomerService.java
+++ MongleProject/src/com/mongle/service/CustomerService.java
@@ -6,8 +6,15 @@ import java.util.Scanner;
 
 import com.mongle.view.MongleVisual;
 
+/**
+ * 고객센터 클래스
+ */
 public class CustomerService {
-
+	/**
+	 * 고객 센터 메뉴
+	 * 
+	 * @return 메뉴 이동을 위한 변수
+	 */
 	public static int csmenu() {
 		Scanner scan = new Scanner(System.in);
 		MongleVisual.menuHeader("고객센터");
@@ -60,60 +67,3 @@ public class CustomerService {
 		return 0;
 	}
 }
-//	private static void inquiryList(ArrayList<Inquiry> list) {
-//		Scanner scan = new Scanner(System.in);
-//
-//		System.out.printf("%22s총 %d개의 문의내역이 존재합니다.\n", " ", list.size());
-//		System.out.println();
-//		for (int i = 0; i < list.size(); i++) {
-//			MongleVisual.menuHeader(i + 1 + "." + list.get(i).getTitleInq());
-//			System.out.printf("%22s%s\n", " ", list.get(i).getTxtInq());
-//			System.out.println();
-//		}
-//		System.out.printf("%22s이전 화면으로 돌아가려면 엔터를 눌러주세요.\n", " ");
-//		scan.nextLine();
-//	}
-//
-//	private static ArrayList<Inquiry> inquiry(ArrayList<Inquiry> list) {
-//		Scanner scan = new Scanner(System.in);
-//		MongleVisual.menuHeader("문의하기");
-//
-//		System.out.printf("%22s문의 제목 : ", " ");
-//		String title = scan.nextLine();
-//		System.out.println();
-//
-//		System.out.printf("%22s문의 내용 : ", " ");
-//		String txt = scan.nextLine();
-//		System.out.println();
-//		Inquiry inq = new Inquiry(title, txt);
-//		list.add(inq);
-//
-//		masking(inq);
-//
-//		System.out.printf("%22s문의가 접수되었습니다. 감사합니다.\n", " ");
-//
-//		System.out.printf("%22s이전 화면으로 돌아가려면 엔터를 눌러주세요.\n", " ");
-//		scan.nextLine();
-//
-//		return list;
-//	}
-//
-//	private static void masking(Inquiry inq) {
-//		String[] forbidden = { "바보", "시발", "개새", "새끼", "멍청이", "병신", "등신", "머저리" };
-//
-//		for (String word : forbidden) {
-//			if (inq.getTitleInq().contains(word)) {
-//				String clean = "";
-//				clean = inq.getTitleInq().replaceAll(word, "**");
-//				inq.setTitleInq(clean);
-//			}
-//			if (inq.getTxtInq().contains(word)) {
-//				String clean = "";
-//				clean = inq.getTxtInq().replaceAll(word, "**");
-//				inq.setTxtInq(clean);
-//			}
-//		}
-//
-//	}
-//
-//}
