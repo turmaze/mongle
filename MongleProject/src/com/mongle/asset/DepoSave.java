53fcd1598c4722f489842a7257bdbd4c7c8e2217
Heey2 <155238712+Heey2@users.noreply.github.com>
2024-02-05 09:48:58 +0900

    주석 처리 추가

--- MongleProject/src/com/mongle/asset/DepoSave.java
+++ MongleProject/src/com/mongle/asset/DepoSave.java
@@ -18,10 +18,19 @@ import com.mongle.service.invest.InfoProduct;
 import com.mongle.view.MongleVisual;
 import com.mongle.yourapp.Encrypt;
 
+/**
+ * 예적금 클래스
+ */
 public class DepoSave {
 	private static String bankDepo;
 	private static String titleDepo;
 
+	/**
+	 * 예적금 생성자
+	 * 
+	 * @param bankDepo  예적금 금융사
+	 * @param titleDepo 예적금 상품 이름
+	 */
 	public DepoSave(String bankDepo, String titleDepo) {
 		super();
 		this.bankDepo = bankDepo;
@@ -29,34 +38,63 @@ public class DepoSave {
 
 	}
 
+	/**
+	 * 예적금 금융사 Getter
+	 * 
+	 * @return 예적금 금융사
+	 */
 	public String getBankDepo() {
 		return bankDepo;
 	}
 
+	/**
+	 * 예적금 금융사 Setter
+	 * 
+	 * @param bankDepo 예적금 금융사
+	 */
 	public void setBankDepo(String bankDepo) {
 		this.bankDepo = bankDepo;
 	}
 
+	/**
+	 * 예적금 상품 이름 Getter
+	 * 
+	 * @return 예적금 상품 이름
+	 */
 	public String getTitleDepo() {
 		return titleDepo;
 	}
 
+	/**
+	 * 예적금 상품 이름 Setter
+	 * 
+	 * @param titleDepo 예적금 상품 이름
+	 */
 	public void setTitleDepo(String titleDepo) {
 		this.titleDepo = titleDepo;
 	}
 
+	/**
+	 * 예적금 toString() String.format("DepoSave [bankDepo=%s, titleDepo=%s]",
+	 * bankDepo, titleDepo);
+	 */
 	@Override
 	public String toString() {
 		return String.format("DepoSave [bankDepo=%s, titleDepo=%s]", bankDepo, titleDepo);
 	}
 
+	/**
+	 * 예적금 상품 검색 및 가입
+	 * 
+	 * @return 메뉴 이동을 위한 변수
+	 */
 	public static int depoSaveService() {
 		Scanner scan = new Scanner(System.in);
 		List<InfoProduct> table = new ArrayList<>(); // 예적금 정보 담을 리스트
 		boolean loop = true;
 
 		while (loop) {
-			table = searchDepoSave(scan, table); // 예적금 검색
+			table = searchDepoSave(table); // 예적금 검색
 
 			System.out.println();
 			System.out.printf("%22s예적금 가입(번호로 선택)\n", " ");
@@ -96,7 +134,14 @@ public class DepoSave {
 		return 0;
 	}
 
-	public static List<InfoProduct> searchDepoSave(Scanner scan, List<InfoProduct> table) { // 예적금 검색 한번에 모으기
+	/**
+	 * 예적금 상품 검색
+	 * 
+	 * @param table 예적금 상품 정보를 담을 리스트
+	 * @return 예적금 상품 정보를 담은 리스트
+	 */
+	public static List<InfoProduct> searchDepoSave(List<InfoProduct> table) { // 예적금 검색 한번에 모으기
+		Scanner scan = new Scanner(System.in);
 		String apiDepo = "https://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth=e06ef138c067a4ff1a42504d0fefda36&topFinGrpNo=020000&pageNo=1";
 		String apiSave = "https://finlife.fss.or.kr/finlifeapi/savingProductsSearch.json?auth=efebe52a92c17a5bcee4c231f829a349&topFinGrpNo=020000&pageNo=1";
 
@@ -110,12 +155,20 @@ public class DepoSave {
 		System.out.printf("%s\n", header);
 		table = searchAPI(table, name, apiDepo);
 		table = searchAPI(table, name, apiSave);
-		printAsciiTable(table);
+		print(table);
 		System.out.printf("%s\n", header);
 
 		return table;
 	}
 
+	/**
+	 * JSON API 분석 및 내용 추출
+	 * 
+	 * @param table 예적금 상품 정보를 담을 리스트
+	 * @param name  검색할 금융사 이름
+	 * @param path  분석할 API path
+	 * @return 예적금 상품 정보를 담은 리스트
+	 */
 	private static List<InfoProduct> searchAPI(List<InfoProduct> table, String name, String path) { // api 검색
 		try {
 			URL url = new URL(path);
@@ -161,13 +214,24 @@ public class DepoSave {
 		return table;
 	}
 
-	public static void printAsciiTable(List<InfoProduct> data) { // 표에 반복해서 출력하는 메서드
+	/**
+	 * 표 형식으로 출력
+	 * 
+	 * @param data 출력할 리스트
+	 */
+	public static void print(List<InfoProduct> data) { // 표에 반복해서 출력하는 메서드
 		for (int i = 0; i < (data.size() > 7 ? 7 : data.size()); i++) {
 			System.out.printf("|%-3d|%-14s|%-20s\t  |%4s개월|%7s%%|%7s%%|\n", i + 1, data.get(i).getBank(),
 					data.get(i).getTitle(), data.get(i).getPeriod(), data.get(i).getRate(), data.get(i).getMaxRate());
 		}
 	}
 
+	/**
+	 * 계좌 개설
+	 * 
+	 * @param bankDepo  예적금 금융사
+	 * @param titleDepo 예적금 상품 이름
+	 */
 	public static void signUp(String bankDepo, String titleDepo) {
 		Scanner sc = new Scanner(System.in);
 		boolean tf = true;
@@ -185,6 +249,12 @@ public class DepoSave {
 
 	}// DepositSignUp
 
+	/**
+	 * 계좌 생성을 위한 데이터 호출
+	 * 
+	 * @param bankDepo  예적금 금융사
+	 * @param titleDepo 예적금 상품 이름
+	 */
 	public static void openDepo(String bankDepo, String titleDepo) {
 		GiveAccount.load();
 
@@ -202,6 +272,9 @@ public class DepoSave {
 
 	}// OpenDeposit
 
+	/**
+	 * 비밀번호 재검사
+	 */
 	public static void Reconfirm() {
 		Scanner sc = new Scanner(System.in);
 
