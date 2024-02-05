b419e0b18db8836ec61c7eb6b5f3a5650fc9e621
Heey2 <155238712+Heey2@users.noreply.github.com>
2024-02-05 10:14:58 +0900

    주석처리 추가

--- MongleProject/src/com/mongle/asset/GiveAccount.java
+++ MongleProject/src/com/mongle/asset/GiveAccount.java
@@ -8,10 +8,19 @@ import java.util.ArrayList;
 
 import com.mongle.resource.BankAccount;
 
+/**
+ * 계좌 생성을 위한 데이터 클래스
+ */
 public class GiveAccount {
 	private static final String BANK = "dat\\계좌 생성 데이터.txt";
+	/**
+	 * 계좌 생성용 데이터가 저장된 리스트
+	 */
 	public static ArrayList<BankAccount> glist = new ArrayList<>();
 
+	/**
+	 * 데이터 파일 로딩
+	 */
 	public static void load() {
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(GiveAccount.BANK));
@@ -29,6 +38,9 @@ public class GiveAccount {
 		}
 	}
 
+	/**
+	 * 데이터 파일 저장
+	 */
 	public static void save() {
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(GiveAccount.BANK));
