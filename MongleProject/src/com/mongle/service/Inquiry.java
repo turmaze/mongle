bb4d52db80bcecc3c538a7f16f472e3928d7e821
Heey2 <155238712+Heey2@users.noreply.github.com>
2024-02-05 17:13:14 +0900

    inq

--- MongleProject/src/com/mongle/service/Inquiry.java
+++ MongleProject/src/com/mongle/service/Inquiry.java
@@ -263,6 +263,7 @@ public class Inquiry {
 	 */
 	public static int checkInq() {
 		Inquiry inq = new Inquiry();
+		inq.loadInq(inqList, ResourcePath.INQUIRY);
 		MongleVisual.menuHeader("문의내역");
 		System.out.printf("%22s총 %d개의 문의내역이 존재합니다.\n", " ", inqList.size());
 		inq.showTxt(inqList);
@@ -629,44 +630,5 @@ public class Inquiry {
 			e.printStackTrace();
 		}
 	}
-
-//	private String titleInq;
-//	private String txtInq;
-//	private String reply;
-//
-//	public String getReply() {
-//		return reply;
-//	}
-//
-//	public void setReply(String reply) {
-//		this.reply = reply;
-//	}
-//
-//	public Inquiry(String titleInq, String txtInq) {
-//		super();
-//		this.titleInq = titleInq;
-//		this.txtInq = txtInq;
-//	}
-//
-//	public String getTitleInq() {
-//		return titleInq;
-//	}
-//
-//	public void setTitleInq(String titleInq) {
-//		this.titleInq = titleInq;
-//	}
-//
-//	public String getTxtInq() {
-//		return txtInq;
-//	}
-//
-//	public void setTxtInq(String txtInq) {
-//		this.txtInq = txtInq;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return String.format("Inquiry [titleInq=%s, txtInq=%s]", titleInq, txtInq);
-//	}
 
 }
