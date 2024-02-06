5f013a76855c5b4eb6121dab05821d28eb64c0ef
bread0808 <155510771+bread0808@users.noreply.github.com>
2024-02-06 15:48:20 +0900

    주석

--- MongleProject/src/com/mongle/resource/AttendList.java
+++ MongleProject/src/com/mongle/resource/AttendList.java
@@ -11,9 +11,9 @@ import com.mongle.view.MongleVisual;
  * */
 public class AttendList {
 	
-	private ArrayList<String> attenddate; //출석일
+	private ArrayList<String> attenddate; //출석일 리스트
 	private String stratedate; //연속 출석일
-	private String emoji; //출석 표시 이모지
+	private String emoji; //출석 마크 이모지
 	
 	/*
 	 * 출석 정보 리스트
@@ -21,7 +21,7 @@ public class AttendList {
 	public static ArrayList<AttendList> list = new ArrayList<>();
 	
 	/*
-	 * 
+	 * 출석 정보 리스트 생성자
 	 * */
 	public AttendList(ArrayList<String> attenddate, String stratedate, String emoji) {
 		this.attenddate = attenddate;
@@ -29,33 +29,66 @@ public class AttendList {
 		this.emoji = emoji;
 	}
 	
+	/**
+	 * 포인트 휙득
+	 */
 	public static void getPoint() {
 		String point = (String) DataBase.getPrivateUser().get(0).get("point");
 		point = (Integer.parseInt(point)+10)+"";
 		
 		DataBase.getPrivateUser().get(0).replace("point", point);
 	}
-
+	
+	/**
+	 * 출석일 리스트 Getter
+	 * 
+	 * @return 출석일 리스트
+	 */
 	public ArrayList<String> getAttenddate() {
 		return attenddate;
 	}
 	
+	/**
+	 * 연속 출석일 Getter
+	 * 
+	 * @return 연속 출석일
+	 */
 	public String getStratedate() {
 		return stratedate;
 	}
-
-	public void setPointdate(ArrayList<String> pointdate) {
-		this.attenddate = pointdate;
+	
+	/**
+	 * 출석일 리스트 Setter
+	 * 
+	 * @return attenddate 출석일 리스트
+	 */
+	public void setAttenddate(ArrayList<String> Attenddate) {
+		this.attenddate = Attenddate;
 	}
-
+	
+	/**
+	 * 연속 출석일 Setter
+	 * 
+	 * @return stratedate 연속 출석일
+	 */
 	public void setStratedate(String stratedate) {
 		this.stratedate = stratedate;
 	}
 	
+	/**
+	 * 출석 마크 Getter
+	 * 
+	 * @return 출석 마크
+	 */
 	public String getEmoji() {
 		return emoji;
 	}
 	
+	/**
+	 * 출석 마크 Setter
+	 * 
+	 * @return emoji 출석 마크
+	 */
 	public void setEmoji(String emoji) {
 		this.emoji = emoji;
 	}
