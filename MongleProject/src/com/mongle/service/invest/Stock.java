860d3e0776962e28b741e8511b8d63f4d7058270
bread0808 <155510771+bread0808@users.noreply.github.com>
2024-02-07 08:42:26 +0900

    1

--- MongleProject/src/com/mongle/service/invest/Stock.java
+++ MongleProject/src/com/mongle/service/invest/Stock.java
@@ -138,6 +138,9 @@ public class Stock {
 				} else if (sel.equals("0")) {
 					MongleVisual.menuMove("이전 화면");
 					return 0;
+				} else {
+					MongleVisual.wrongInput();
+					continue;
 				}
 				System.out.println();
 
